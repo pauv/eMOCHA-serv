8b7f310bfcdd60f7df35651e12b4ee76ccd3ce69
George Graham <george@ggraham.net>
2011-02-21 12:33:51 +0000

    form update times

--- modules/emocha/classes/api.php
+++ modules/emocha/classes/api.php
@@ -23,10 +23,11 @@ class Api
 			$times['last_form_template_upd'] = $row['last_updated']==NULL?0:$row['last_updated'];
 			
 			// form config
-			$sql = "SELECT UNIX_TIMESTAMP(MAX(last_modified)) as last_updated FROM forms";
+			$sql = "SHOW TABLE STATUS LIKE 'forms'";
     		$result = DB::query(Database::SELECT, $sql)->execute();
 			$row = $result->current();
-			$times['last_form_config_upd'] = $row['last_updated']==NULL?0:$row['last_updated'];
+			$times['last_form_config_upd']=strtotime($row['Update_time']);
+
 			
 			// app config
 			$sql = "SELECT UNIX_TIMESTAMP(MAX(last_modified)) as last_updated FROM configs
