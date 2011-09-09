ed2eaf7d542624ec46cc01f473f09f85ffe1e67e
George Graham <george@ggraham.net>
2011-09-09 10:15:08 +0200

    add graph to stats nav

--- modules/emocha/views/stats/nav.php
+++ modules/emocha/views/stats/nav.php
@@ -2,3 +2,4 @@
 	<li id="nav_households"><a href="<?php echo Url::site('stats/households') ?>">data by household</a></li>
 	<li id="nav_export"><a href="<?php echo Url::site('stats/exportgrid') ?>">export</a></li>
 	<li id="nav_referrals"><a href="<?php echo Url::site('stats/referrals') ?>">referrals</a></li>
+	<li id="nav_timegraph"><a href="<?php echo Url::site('stats/timegraph') ?>">graph</a></li>
