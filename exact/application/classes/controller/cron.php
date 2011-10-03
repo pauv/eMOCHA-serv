eb19ab275c606f76c8ffaec774fbea66ca9137ac
George Graham <george@ggraham.net>
2011-10-03 10:14:02 +0200

    testing kohana cron

---
+++ exact/application/classes/controller/cron.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Cron extends Controller {
+
+	
+	
+	 public function action_test()
+	{
+	
+		echo "this works";
+	
+	}
+	
+	
+}
