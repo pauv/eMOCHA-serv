752e4d50afc9ee41e2e65f872aff94a28567990a
George Graham <george@ggraham.net>
2011-10-24 10:01:05 +0200

    admin nav

--- exact/application/views/admin/nav.php
+++ exact/application/views/admin/nav.php
@@ -1,5 +1,6 @@
 	<li id="nav_new_users"><a href="<?php echo Url::site('admin/new_users') ?>">new users</a></li>
 	<li id="nav_forms"><a href="<?php echo Url::site('admin/forms') ?>">forms</a></li>
 	<li id="nav_phones"><a href="<?php echo Url::site('admin/phones') ?>">phones</a></li>
+	<li id="nav_configs"><a href="<?php echo Url::site('admin/configs') ?>">configs</a></li>
 	<li id="nav_alarms"><a href="<?php echo Url::site('admin/patients') ?>">studies/patients</a></li>
 	
