a0eeb6e8a5e7741bcf739882a324c310d73b170c
George Graham <george@ggraham.net>
2011-11-17 11:02:56 +0100

    db changes

--- install/db-update-20111108-george.sql
+++ install/db-update-20111108-george.sql
@@ -1,1 +1,11 @@
-ALTER TABLE `c2dm_errors` ADD `phone_id` INT NOT NULL;
+CREATE TABLE `c2dm_errors` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `curl_error` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `http_code` int(11) NOT NULL,
+  `response` text COLLATE utf8_unicode_ci NOT NULL,
+  `data` text COLLATE utf8_unicode_ci NOT NULL,
+  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `phone_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
--- install/db-update-20111110-george.sql
+++ install/db-update-20111110-george.sql
@@ -2,3 +2,4 @@ ALTER TABLE `configs` ADD `description` VARCHAR( 255 ) NOT NULL AFTER `content`;
 ALTER TABLE `configs` CHANGE `description` `description` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
 
 ALTER TABLE `phone` ADD `c2dm_disable` TINYINT NOT NULL;
+ALTER TABLE `phone` CHANGE `c2dm_disable` `c2dm_disable` TINYINT( 1 ) NOT NULL;
