9ddbff01812766efc150eb80e96c7006ab79101d
George Graham <george@ggraham.net>
2012-01-04 10:47:58 +0100

    demo messages submenu

---
+++ demo/application/views/messages/nav.php
@@ -0,0 +1,3 @@
+	<li id="nav_send"><a href="<?php echo Url::site('messages/send') ?>">C2dm</a></li>
+	<li id="nav_patients"><a href="<?php echo Url::site('sms/send/patients') ?>">Patients</a></li>
+	<li id="nav_CHWs"><a href="<?php echo Url::site('sms/send/CHWs') ?>">CHWs</a></li>
