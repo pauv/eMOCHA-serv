0d800cc9333f0f57e911ac0640b1cf74db5040d1
George Graham <george@ggraham.net>
2012-01-19 13:10:27 +0100

    tbdetect language integration

--- tbdetect/application/classes/controller/handsets.php
+++ tbdetect/application/classes/controller/handsets.php
@@ -10,4 +10,17 @@ class Controller_Handsets extends Emocha_Controller_Handsets {
 		$content->phones = ORM::factory('phone')->where('language','=',$this->language)->find_all();
 
 	}
+	
+	public function action_location() {
+		
+		$this->template->title = 'Locations';	
+		$content = $this->template->content = View::factory('handsets/location');
+		
+		$phones = ORM::factory('phone')->where('language','=',$this->language)->find_all();
+		$markerDataJS = $this->_get_gmaps_js($phones);
+		$content->gmaps_js = View::factory('googlemaps/gmaps_js', array(
+									'markerDataJS' => $markerDataJS,
+									'google_maps_key' => Kohana::config('googlemaps.key')
+									));		
+	}
 }
