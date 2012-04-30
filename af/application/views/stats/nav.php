cfde0be23d65d8f9f4736801cb83cafb85c60b9d
Pau Varela <pau.varela@gmail.com>
2012-04-30 21:56:53 +0200

    finish application_type stats/data visualization for customized projects

--- af/application/views/stats/nav.php
+++
@@ -1,2 +0,0 @@
-	<li id="nav_data"><a href="<?php echo Url::site('stats/data') ?>">data by patient</a></li>
-	<li id="nav_export"><a href="<?php echo Url::site('stats/exportgrid') ?>">export forms</a></li>
--- exact/application/views/stats/nav_forms_only.php
+++ exact/application/views/stats/nav_forms_only.php
@@ -1,3 +1,3 @@
-	<li id="nav_data"><a href="<?php echo Url::site('stats/data') ?>">data by patient</a></li>
+	<li id="nav_data_forms_only"><a href="<?php echo Url::site('stats/data_forms_only') ?>">data by patient</a></li>
 	<li id="nav_export"><a href="<?php echo Url::site('stats/export') ?>">export forms</a></li>
 	<li id="nav_export_gps"><a href="<?php echo Url::site('stats/export_gps') ?>">export gps</a></li>
