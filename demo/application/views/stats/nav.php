43594a5e3c01a9f82bf46783b01e8c014ca4e383
George Graham <george@ggraham.net>
2012-07-20 16:50:18 +0200

    SERV-3: first steps, adding links to demo nav

--- demo/application/views/stats/nav.php
+++ demo/application/views/stats/nav.php
@@ -5,3 +5,5 @@
 	<li id="nav_timegraph"><a href="<?php echo Url::site('stats/timegraph') ?>">graph</a></li>
 	<li id="nav_piechart"><a href="<?php echo Url::site('stats/piechart') ?>">symptom pie chart</a></li>
 	<li id="nav_symptomgraph"><a href="<?php echo Url::site('stats/symptomgraph') ?>">symptom graph</a></li>
+	<li id="nav_piecharts"><a href="<?php echo Url::site('stats/piecharts') ?>">pie charts</a></li>
+	<li id="nav_dategraphs"><a href="<?php echo Url::site('stats/dategraphs') ?>">date graphs</a></li>
