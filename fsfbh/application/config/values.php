a39836ad1bbcf74ba149e05391252a7554107b52
Pau Varela <pau.varela@gmail.com>
2012-12-16 22:59:54 +0100

    delete unused files

--- fsfbh/application/config/values.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
-return array(
-	//form codes
-	'hcore_form_code' => 'hcore',
-	'epub_form_code' => 'epub',
-	'pcore_form_code' => 'pcore'
-);
